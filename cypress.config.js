49eb49a49e8e82b4c929952a6494c4a136dc37be
Marek Szumny <lukmarcus@gmail.com>
2023-05-15 12:27:19 +0200

    test02a

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const { cloudPlugin } = require("cypress-cloud/plugin");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      return cloudPlugin(on, config);
     },
   },
 });
