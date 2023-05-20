d03846f0567a26635cf729e9c313aa764d769191
Marek Szumny <lukmarcus@gmail.com>
2023-05-20 11:41:29 +0200

    projectId fixed

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "zkiptr",
+  projectId: "hbq2gb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
