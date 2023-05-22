9cba83ffa3b34ce45b15fe050d7660c4b40825cb
Marek Szumny <lukmarcus@gmail.com>
2023-05-22 23:05:08 +0200

    projectID disabled in config

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "hbq2gb",
+  // projectId: "hbq2gb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
